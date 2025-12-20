5d50ee7f8e9987da00680684f8bc2c849b877ad9
MrNizzy <mr.nizzy.0128@gmail.com>
2025-12-20 15:22:34 -0500

    feat: add ngx-icons-extra integration to demo app
    - Added npm scripts for library build and watch mode in package.json
    - Configured HttpClient with fetch API in demo app configuration
    - Replaced Angular landing page template with ngx-icons-extra icon component
    - Integrated Tabler icons collection with home icon display (200x200)
    - Added HTTP client injection and OnInit lifecycle hook to app component

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxIcon } from 'ngx-icons-extra';
+
+interface CollectionDetails {
+  name: string;
+  total: number;
+  version: string;
+  author: {
+    name: string;
+    url: string;
+  };
+  license: {
+    title: string;
+    spdx: string;
+    url: string;
+  };
+  samples: string[];
+  height: number;
+  displayHeight: number;
+  category: string;
+  palette: boolean;
+}
+
+type CollectionsResponse = Record<string, CollectionDetails>;
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxIcon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('demo');
+  readonly http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get<CollectionsResponse>('https://api.iconify.design/collections').subscribe((res) => {
+      const collectionIds = Object.keys(res);
+      console.log(collectionIds);
+    });
+    
+  }
 }
--- projects/ngx-icons-extra/src/lib/ngx-icons-extra.ts
+++ projects/ngx-icons-extra/src/lib/ngx-icons-extra.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { IconCollections } from '../models/collections';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
-  selector: 'lib-ngx-icons-extra',
+  selector: 'ngx-icon',
   imports: [],
-  template: `
-    <p>
-      ngx-icons-extra works!
-    </p>
-  `,
-  styles: ``,
+  template: ` <span [innerHTML]="svg()"></span> `,
 })
-export class NgxIconsExtra {
+export class NgxIcon {
+  private readonly http = inject(HttpClient);
+  private readonly sanitizer = inject(DomSanitizer);
+  public readonly collection = input.required<IconCollections>();
+  public readonly icon = input.required<string>();
+  public readonly width = input<string>('20');
+  public readonly height = input<string>('20');
+  public readonly color = input<string>('');
+  public readonly flip = input<string>('');
+  public readonly rotate = input<string>('');
 
+  svg = signal<SafeHtml | null>(null);
+
+  ngOnInit(): void {
+    const query = new HttpParams()
+      .set('width', this.width())
+      .set('height', this.height())
+      .set('color', this.color())
+      .set('flip', this.flip())
+      .set('rotate', this.rotate());
+    this.http
+      .get(
+        `https://api.iconify.design/${this.collection()}/${this.icon()}.svg?${query.toString()}`,
+        {
+          responseType: 'text',
+        }
+      )
+      .subscribe((svg) => {
+        this.svg.set(this.sanitizer.bypassSecurityTrustHtml(svg));
+      });
+  }
 }
